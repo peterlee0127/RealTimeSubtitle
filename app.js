80049c094d2801cba1de2f3341a542f226f85f5b
PeterLee <peterlee0127@gmail.com>
2017-09-28 14:18:24 +0800

    re modify server design

--- app.js
+++ app.js
@@ -1,84 +1,21 @@
 var express = require('express');
-const fileUpload = require('express-fileupload');
 var app = express();
+var helmet = require('helmet')
 var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
-var fs = require('fs');
-var message = "";
-var showStatus = false;
+
+var server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
+app.use(helmet())
+
+var api = require('./routes/api')(io);
+app.use('/',api);
+
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-  res.render('index', { title: message,status:showStatus });
-
-})
-app.get('/admin', (req, res) => {
-
-  res.render('admin', { title: message,status:showStatus});
-
-});
-
-
-
-app.get('/api/list', (req, res) => {
-  var json = fs.readFileSync("./public/list.json", "utf8");
-  if (json) {
-    res.json(json);
-  } else {
-    res.json({});
-  }
-});
-
-
-app.use(fileUpload());
-
-app.post('/upload/image', function (req, res) {
-  if (!req.files.pic) {
-    return res.status(400).send('No files were uploaded.');
-  } else {
-    let file = req.files.pic;
-    handlefile(file, res,'./public/back.png');
-  }
-});
-
-app.post('/upload/json', function (req, res) {
-  if (!req.files.json) {
-    return res.status(400).send('No files were uploaded.');
-  } else {
-    let file = req.files.json;
-    handlefile(file, res,'./public/list.json');
-  }
-});
-
-function handlefile(file, res,save_path) {
-  file.mv(save_path, function (err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
-  });
-  setTimeout(function () {
-
-    res.redirect("/admin")
-  }, 500);
-}
-
-io.on('connection', function (socket) {
-  socket.on('title', function (data) {
-    message = data.title;
-    io.emit('new title', { title: message });
-  });
-  socket.on('status',function(data){
-    showStatus = data.status;
-    io.emit('new status',{ status: showStatus }) 
-  })
-});
-
-
---
+++ routes/api.js
@@ -0,0 +1,75 @@
+module.exports = function(io){
+
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+
+let message = "";
+let showStatus = false;
+
+
+router.get('/', (req, res) => {
+  res.render('index', { title: message,status:showStatus });
+})
+
+router.get('/admin', (req, res) => {
+  res.render('admin', { title: message,status:showStatus});
+});
+
+
+
+router.get('/api/list', (req, res) => {
+  var json = fs.readFileSync("./public/list.json", "utf8");
+  if (json) {
+    res.json(json);
+  } else {
+    res.json({});
+  }
+});
+
+
+router.use(fileUpload());
+
+router.post('/upload/image', function (req, res) {
+  if (!req.files.pic) {
+    return res.status(400).send('No files were uploaded.');
+  } else {
+    let file = req.files.pic;
+    handlefile(file, res,'./public/back.png');
+  }
+});
+
+router.post('/upload/json', function (req, res) {
+  if (!req.files.json) {
+    return res.status(400).send('No files were uploaded.');
+  } else {
+    let file = req.files.json;
+    handlefile(file, res,'./public/list.json');
+  }
+});
+
+function handlefile(file, res,save_path) {
+  file.mv(save_path, function (err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+  });
+  setTimeout(function () {
+
+    res.redirect("/admin")
+  }, 500);
+}
+
+io.on('connection', function (socket) {
+  socket.on('title', function (data) {
+    message = data.title;
+    io.emit('new title', { title: message });
+  });
+  socket.on('status',function(data){
+    showStatus = data.status;
+    io.emit('new status',{ status: showStatus }) 
+  })
+});
+    return router;
+}
