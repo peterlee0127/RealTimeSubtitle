dd2d5aadbeb599175d1843e8d982b5b67ce16697
HIO-PRO\a8993 <shbhsu@gmail.com>
2017-09-30 15:53:18 +0800

    display switch

--- app/req.js
+++ app/req.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 require('bootstrap');
 require('jquery');
 require('webpack-jquery-ui/draggable');
@@ -23,9 +24,9 @@ function BindListData(){
 
 function TopSwitch(props) {
   return (
-    <div>
+    <div className="material-switch">
       <input id="DepartdisplaySwitch" onClick={BindListData} name={props.name} type="checkbox"></input>
-      <label for="DepartdisplaySwitch" class="label-primary">{props.name}</label>
+      <label for="DepartdisplaySwitch" className="label-primary">{props.name}</label>
     </div>
  );
 }
@@ -36,6 +37,36 @@ ReactDOM.render(
   <TopSwitch name='Depart Display'/>],
   document.getElementById('setting-switch')
 );
+
+
+function DisplaySwitch() {
+  return (
+ 
+    <div className="row">
+              <div className="col-lg-12 ">
+                <div> Display</div>
+                <button type="button" className="btn btn-sm btn-danger" onclick="$('#displaySwitch').click();changeShowStatus()"> Ctrl + `</button>
+
+              </div>
+              <div className="col-lg-12 ">
+
+                <div className="material-switch pull-right">
+                  <input id="displaySwitch" onclick="changeShowStatus()" name="displaySwitch" type="checkbox" />
+                  <label for="displaySwitch" className="label-primary"></label>
+                </div>
+              </div>
+
+            </div> 
+ );
+}
+
+
+
+ReactDOM.render(
+ DisplaySwitch(),
+  document.getElementById('display_Switch')
+);
+
 
 // const deparDiv = <div>Dept. Display</div>
 //               <div class="col-lg-4">
