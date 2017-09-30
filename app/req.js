1f7f27e2dd45d098b29a4d805d5f1814acb8cc7a
PeterLee <peterlee0127@gmail.com>
2017-09-30 15:22:19 +0800

    add test switchUI

--- app/req.js
+++ app/req.js
@@ -1,3 +1,5 @@
+'use strict';
+//
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,28 +8,49 @@ require('jquery');
 require('webpack-jquery-ui/draggable');
 require('webpack-jquery-ui/resizable');
 
+// function TopSwitchUI(props) {
+//     return (
+//       <div class="col-lg-4">
+//         <div>{props.name}
+//         <div class="material-switch pull-right">
+//     )
+// }
 
-function TopSwitch(props) {
-    return <div class="material-switch pull-right"><input id="DepartdisplaySwitch" onclick="BindListData()" name="displaySwitch" type="checkbox" <%=DepartStatus? "checked"  : "" %>/> <label for="DepartdisplaySwitch" class="label-primary"></label>
-        </div>
-      </div>
-
+function BindListData(){
+  alert("dd");
 }
 
-const deparDiv = <div>Dept. Display</div>
-              <div class="col-lg-4">
-        <div>Name Display</div>
-        <div class="material-switch pull-right">
-          <input id="NamedisplaySwitch" onclick="BindListData()" name="displaySwitch" type="checkbox" <%=NameStatus? "checked" : ""
-            %>/>
-          <label for="NamedisplaySwitch" class="label-primary"></label>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div>Job Display</div>
-        <div class="material-switch pull-right">
-          <input id="JobdisplaySwitch" onclick="BindListData()" name="displaySwitch" type="checkbox" <%=JobStatus? "checked" : "" %>/>
-          <label for="JobdisplaySwitch" class="label-primary"></label>
-        </div>
- 
 
+function TopSwitch(props) {
+  return (
+    <div>
+      <input id="DepartdisplaySwitch" onClick={BindListData} name={props.name} type="checkbox"></input>
+      <label for="DepartdisplaySwitch" class="label-primary">{props.name}</label>
+    </div>
+ );
+}
+
+ReactDOM.render(
+  [<TopSwitch name='Depart Display'/>,
+  <TopSwitch name='Name Display'/>,
+  <TopSwitch name='Depart Display'/>],
+  document.getElementById('setting-switch')
+);
+
+// const deparDiv = <div>Dept. Display</div>
+//               <div class="col-lg-4">
+//         <div>Name Display</div>
+//         <div class="material-switch pull-right">
+//           <input id="NamedisplaySwitch" onclick="BindListData()" name="displaySwitch" type="checkbox"
+//       //    <%=NameStatus? "checked" : "" %>/>
+//           <label for="NamedisplaySwitch" class="label-primary"></label>
+//         </div>
+//       </div>
+//       <div class="col-lg-4">
+//         <div>Job Display</div>
+//         <div class="material-switch pull-right">
+//           <input id="JobdisplaySwitch" onclick="BindListData()" name="displaySwitch" type="checkbox" <%=JobStatus? "checked" : "" %>/>
+//           <label for="JobdisplaySwitch" class="label-primary"></label>
+//         </div>
+//         // <TopSwitchUI name='Depart Display' onclick='BindListData()'>
+//         // <TopSwitchUI name="Name Display"> onclick='BindListData()'>,
