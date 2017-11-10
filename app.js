149ad84857e7789978f1f76f6d994dc4ac7586e7
PeterLee <peterlee0127@gmail.com>
2017-11-10 13:18:34 +0800

    add subtitle api

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ let port = process.env.PORT || 8080;
 let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-
 
 let api = require('./routes/api')(io);
 app.use('/', api);
